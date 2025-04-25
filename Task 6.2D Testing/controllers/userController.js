14e6b56e9d44667066599c9af988d0f091d27de2
SujayA-git <82646124+SujayA-git@users.noreply.github.com>
2025-04-25 10:04:50 +1000

    commit_testing

--- Task 6.2D Testing/controllers/userController.js
+++ Task 6.2D Testing/controllers/userController.js
@@ -28,6 +28,28 @@ exports.addContent = async (req, res) => {
             return res.status(400).send("Invalid email format");
         }
 
+        const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!]).{8,}$/; //ensuring good password strength
+        if (!passwordPattern.test(password)) {
+            return res.status(400).send("Password must be at least 8 characters long and include atleast \n 1 uppercase,\n 1 lowercase,\n a number,\n a special character.");
+        }
+
+
+        const existing = await content.findOne({ email });
+        if (existing) {
+            return res.status(409).send("error! User with same email already exists");
+        }
+
+        if (
+            !first_name.trim() || !last_name.trim() || !email.trim() || !password.trim()
+        ) {
+            return res.status(400).send("Detail Entries shouldn't be blank or whitespaces");
+        }
+        
+        const nameRegex = /^[a-zA-Z\s]+$/;
+        if (!nameRegex.test(first_name) || !nameRegex.test(last_name)) {
+            return res.status(400).send("First and Last Names can contain only alphabets");
+        }
+
         const sampleProject = new content({ first_name, last_name, password, email });
         await sampleProject.save();
         console.log("content saved!");
